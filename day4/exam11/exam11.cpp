849ed41530808f2c4c69200ef562c976fe687f58
onec555 <onec555@gmail.com>
2017-06-26 16:22:05 +0900

    입출력 함수 예제

---
+++ day4/exam11/exam11.cpp
@@ -0,0 +1,23 @@
+// exam11.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	//Exercise 3-2 (87.page) Vitamin 야구단
+
+	int nNum, nAge;
+	double dbHitRate;
+
+	printf("등번호를 입력하세요:");  scanf_s("%d",&nNum);
+	printf("타율을 입력하세요:");  scanf_s("%lf",&dbHitRate);
+	printf("나이를 입력하세요:");  scanf_s("%d",&nAge);
+
+	printf("%d번 선수의 타율은 %lf, 나이는 %d입니다.\n", nNum, dbHitRate, nAge);
+
+
+    return 0;
+}
+
